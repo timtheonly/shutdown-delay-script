3a0b233b4c5b4766892cb369b350bd8755d92eeb
daniel hogan <timtheonly@gmail.com>
2012-09-08 18:15:36 +0100

    switched around if's

--- cleanShutdownlog.sh
+++ cleanShutdownlog.sh
@@ -1,23 +1,22 @@
 #! /bin/bash
-logFile=shutdown.log
-if [$# -eq 0]
+logFile=/home/hogan/shutdown.log
+if [ $# -gt 0 ]
 then 
-	size=$(stat -c %s $logFile)
-	#echo $logFile $size
-	if test $size -ge 3145728
-		then
+	if [ $1 =  "clean" ]
+	then
+			rm $logFile
+			echo "`date +%a-%H:%M:%S-%d/%m/%y`--  log file forceably cleaned" >> $logFile
+			exit
+	fi
+fi
+
+
+size=$(stat -c %s $logFile)
+if [ $size -ge 3145728 ] 
+	then
 		tail $logFile >> tmp
 		rm  $logFile
 		echo "`date +%a-%H:%M:%S-%d/%m/%y`--  log file cleaned" >> $logFile
 		cat tmp >> $logFile
 		exit
-	fi
 fi
-
-if test $1 = "clean"
-	then
-	rm $logFile
-	echo "`date +%a-%H:%M:%S-%d/%m/%y`--  log file forceably cleaned" >> $logFile
-	exit
-fi
-
