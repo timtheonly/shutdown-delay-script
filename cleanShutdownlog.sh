fe3052f9e77993be9b90a2af17fccc61117dca45
daniel hogan <theonlytim@hotmail.com>
2012-09-08 15:57:40 +0100

    added log file cleaner script

---
+++ cleanShutdownlog.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+logFile=shutdown.log
+size=$(stat -c %s $logFile)
+#echo $logFile $size
+if test $size -ge 3145728
+	then
+	tail $logFile >> tmp
+	rm  $logFile
+	echo "`date +%a-%H:%M:%S-%d/%m/%y`--  log file cleaned" >> $logFile
+	cat tmp >> $logFile
+	exit
+fi
+
+if test $1 = "clean"
+	then
+	rm $logFile
+	echo "`date +%a-%H:%M:%S-%d/%m/%y`--  log file forceably cleaned" >> $logFile
+	exit
+fi
+
